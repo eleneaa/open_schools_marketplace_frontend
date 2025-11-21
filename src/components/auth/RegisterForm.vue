4fdefd8e8df7ac5a748c448737f09c9828e3f164
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-11-22 00:49:02 +0500

    Created RegisterForm and added nested route into /auth

---
+++ src/components/auth/RegisterForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col gap-6 w-full">
+    <div class="flex flex-col gap-2">
+      <label for="fullname">ФИО</label>
+      <AuthInput id="fullname" type="text" ph="Иванов Иван Иванович" />
+      <label for="email">Email</label>
+      <AuthInput id="email" type="email" ph="example@mail.com" />
+      <label for="password">Придумайте пароль</label>
+      <AuthInput id="password" type="password" ph="Пароль" />
+      <label for="cpassword">Повторите пароль</label>
+      <AuthInput id="cpassword" type="password" ph="Пароль" />
+    </div>
+
+    <button class="bg-[#2698FF] rounded-lg text-white p-2">Зарегистрироваться</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AuthInput from './AuthInput.vue';
+
+
+</script>
+
+<style scoped></style>
