2d232169ae461c2d1b83bbf88130fecbfd545127
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-11-22 01:51:39 +0500

    added logs for inputs and redirect to profile after sending form

--- src/components/auth/AuthInput.vue
+++ src/components/auth/AuthInput.vue
@@ -1,10 +1,14 @@
 <template>
-  <input :id="id" :type="type" :placeholder="ph"
+  <input :id="id" :type="type" :placeholder="ph" :value="modelValue"
+    @input="(e: Event) => $emit('update:modelValue', (e.target as HTMLInputElement).value)"
     class="border border-[#D9D9D9] rounded-lg p-2 outline-[#207BCE] placeholder:text-[#00000040]" />
 </template>
 
 <script setup lang="ts">
-defineProps<{ id: string, type: string, ph: string }>();
+defineProps<{ id: string, type: string, ph: string, modelValue?: string }>();
+defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
 </script>
 
 <style scoped></style>
