0cc67d2226b582f1aa446738f5a978894999dc74
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-11-22 13:27:47 +0500

    added valiadtion to LoginForm

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,10 +1,12 @@
 <template>
-  <form @submit.prevent="submit" class="flex flex-col gap-6 w-full">
+  <form @submit.prevent="onSubmit" class="flex flex-col gap-6 w-full">
     <div class="flex flex-col gap-2">
-      <label for="login">Логин</label>
-      <AuthInput v-model="loginValue" id="login" type="text" ph="UsernameExmaple" />
-      <label for="password">Пароль</label>
-      <AuthInput v-model="passwordValue" id="password" type="password" ph="Пароль" />
+      <label for="login">Логин <span v-if="loginMeta.touched" class="text-orange-900 text-xs">{{ loginError
+          }}</span></label>
+      <AuthInput v-model="login" id="login" type="text" ph="UsernameExmaple" />
+      <label for="password">Пароль <span v-if="passwordMeta.touched" class="text-orange-900 text-xs">{{ passwordError
+      }}</span></label>
+      <AuthInput v-model="password" id="password" type="password" ph="Пароль" />
       <RouterLink to="register" class="text-sm">Забыли пароль?</RouterLink>
     </div>
 
@@ -14,21 +16,37 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { z } from 'zod';
+import { toTypedSchema } from '@vee-validate/zod';
 import AuthInput from './AuthInput.vue';
+import { useField, useForm } from 'vee-validate';
 
 const router = useRouter()
 
-const loginValue = ref()
-const passwordValue = ref()
+const schema = toTypedSchema(
+  z.object({
+    login: z.string().nonempty('Обязательное поле'),
+    password: z.string().nonempty('Обязательное поле').min(6, 'Минимум 6 символов')
+  })
+)
 
-function submit() {
-  console.log('Логин:', loginValue.value)
-  console.log('passwd:', passwordValue.value)
+const { handleSubmit } = useForm({
+  initialValues: {
+    login: "",
+    password: "",
+  },
+  validationSchema: schema,
+  validateOnMount: false
+})
+
+const { value: login, errorMessage: loginError, meta: loginMeta } = useField<string>('login')
+const { value: password, errorMessage: passwordError, meta: passwordMeta } = useField<string>('password')
+
+const onSubmit = handleSubmit((values) => {
+  console.log(values)
   router.push('/profile')
-}
-
+})
 </script>
 
 <style scoped></style>
